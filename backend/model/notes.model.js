7ee22e6899809f18338eed62cbe87ba6a05b515a
Deepak <deepak2603dk@gmail.com>
2023-12-06 11:21:34 +0530

    notes model

---
+++ backend/model/notes.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose;
+
+const postSchema = new Schema(
+    {
+      caption: {
+        type: String,
+        minlength: 1,
+        required: true,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      pdf: {
+        //we will handle later  upload pdf on cloudinary
+        public_id: String,
+        url: String,
+      },
+      owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      likes: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+        },
+      ],
+      comments: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+          Comment: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+    { timestamps: true }
+  );
+
+
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = { Post };
