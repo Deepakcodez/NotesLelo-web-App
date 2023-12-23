16c44ae30aa1f57f0cfea838d6025d70128c6fd0
Deepak <deepak2603dk@gmail.com>
2023-12-23 16:50:33 +0530

    popUp on click functionality

--- frontend/src/Components/shared/Navbar.jsx
+++ frontend/src/Components/shared/Navbar.jsx
@@ -3,12 +3,16 @@ import { NavLink } from "react-router-dom";
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 import { GoPlus } from "react-icons/go";
 
-
 function Navbar(props) {
   const { name, email } = props.userDetail; //destructure the props(object) inner object calles userDetail
   // console.log('>>>>>>>>>>>name', name[0].toUpperCase())
   const [avatarSign, setAvatarSign] = useState();
+  const [ispopUp, setPopUp] = useState(false);
+  
 
+  const addHandler = () => {
+    setPopUp(!ispopUp)
+  };
   useEffect(() => {
     const firstCharacter = name ? name[0] : ""; // Check if name is defined
     const capitalAvatarcharacter =
@@ -18,15 +22,28 @@ function Navbar(props) {
   }, [name]);
 
   return (
-    <section className="top ">
+    <section className="top  ">
       <div
-      style={{}}
-      className="flex justify-between gap-3 py-4 items-center px-3 bg-slate-800 border-b-[1px] border-slate-700">
+        style={{}}
+        className="flex justify-between gap-3 py-4 items-center px-3 bg-slate-800 border-b-[1px] border-slate-700"
+      >
         <NavLink to={"/"} className="font-bold text-xl text-white ">
           <img src="/src/assets/logo.png" className="h-[2rem]"></img>{" "}
         </NavLink>{" "}
-        <section className="rightnav flex items-center gap-3 ">
-          <GoPlus className="text-3xl text-white bg-slate-700 rounded-full p-[.2rem] hover:bg-slate-600"/>
+        <section className="rightnav flex items-center gap-3 relative ">
+          <GoPlus
+            className="text-4xl text-white bg-transparent rounded-full p-[.2rem] hover:bg-slate-700"
+            onClick={addHandler}
+          />
+          {/* popup */}
+          {ispopUp&&<div className="popup absolute bg-slate-400 text-white shadow-lg py-4 w-[8rem]  top-8 end-10 rounded-sm  ">
+            <ul className="flex flex-col items-center">
+              <li className="hover:bg-slate-500 w-full cursor-pointer ps-4"> <h1>Create Group</h1></li>
+              <li className="hover:bg-slate-500 w-full cursor-pointer ps-4 "> <h1>Join Group</h1></li>
+            </ul>
+          </div>}
+          
+
           <div className="h-10 w-10 rounded-full bg-blue-300 items-center justify-center flex  text-white font-semibold text-2xl ">
             {avatarSign}
           </div>
