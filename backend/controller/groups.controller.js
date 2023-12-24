84d6cc855bf5fb4f74329dc90ea23456b563bad3
Deepak <deepak2603dk@gmail.com>
2023-12-24 13:02:16 +0530

    storing group data to the database

--- backend/controller/groups.controller.js
+++ backend/controller/groups.controller.js
@@ -3,14 +3,14 @@ const groupModel=groupdb.Group;
 
 
 
-const demo= async(req,res)=>{
+const demo= async(req,resp)=>{
   try{
-    res.send({
+    resp.send({
         status:200,
         message:"ready to use"
       });
   }catch(err){
-    res.send({
+    resp.send({
         err
 
       });
@@ -18,22 +18,48 @@ const demo= async(req,res)=>{
 };
 
 
-const createGroup = async (req,res)=>{
-    const{tittle,description} = req.body;
-    if(!tittle){
-        return res.status(422).json({
-            status:422,
-            success:false,
-            error:"Enter tittle of the group "
-        });
-    }
-    else{
-        return res.status(200).json({
-            status:200,
-            success:true,
-            Message:"Group Created Successfullly",
-        });
-    }
+
+// create group controller 
+
+const createGroup = async (req,resp)=>{
+    const{title,description} = req.body;
+  try {
+      if(!title){
+          return resp.status(422).json({
+              status:422,
+              success:false,
+              error:"Enter tittle of the group "
+          });
+      }
+      const existingGroup = await groupModel.findOne({ title: title });
+
+    if (existingGroup) {
+      return resp.status(400).json({
+        status: 400,
+        success: false,
+        message: "Group already exist already exists",
+      });
+    } 
+      else{
+        const group = new groupModel({
+            title,
+            description
+        
+          });
+    
+          const storeGroupData = await group.save();
+    
+          console.log(">>>>>>>>>>> Register successful");
+          return resp.status(200).json({
+              status:200,
+              success:true,
+              Message:"Group Created Successfullly,",
+              data:storeGroupData
+          });
+      }
+  } catch (error) {
+    
+  }
 
     
 }
