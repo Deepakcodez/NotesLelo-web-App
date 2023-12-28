017eaf1dcbbc2edd9a45210f04b87dccbc3b28c7
Deepak <deepak2603dk@gmail.com>
2023-12-29 02:21:21 +0530

    fixing css

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -5,7 +5,7 @@ const Home=()=> {
 
   return (
     <>
-    <div className="pt-20 px-7    w-full   gap-10  grid justify-center overflow-y-scroll no-scrollbar  pb-[30rem]"
+    <div className="pt-20 px-[.5rem] md:px-[6rem]  justify-center md:justify-start   w-full   gap-10  grid  overflow-y-scroll no-scrollbar  pb-[30rem]"
     style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(5rem , 20rem))' }}
 
     >
