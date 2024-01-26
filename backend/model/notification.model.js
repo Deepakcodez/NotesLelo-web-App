dd8ed2c76f849657e4544fadeaa545627b5547c8
Deepak <deepak2603dk@gmail.com>
2024-01-27 00:06:18 +0530

    modifying notification model

--- backend/model/notification.model.js
+++ backend/model/notification.model.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose')
-const notificationSchma = new mongoose.Schema({
-    notification:[{
-        user:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref : 'User',
-            
-            },
-        receiver:{ 
+const notificationSchema = new mongoose.Schema({
+    
+        
+        user:{ 
             type : mongoose.Schema.Types.ObjectId,
             ref : 'User',
            
             },
         message:{
             type : String,
-           
+           required : true,
         }
-    }]
    
 },
  {timestamps:true}
 );
 
-const notification = mongoose.model('notification',notificationSchma);
+const notification = mongoose.model('notification',notificationSchema);
 
 module.exports ={notification};
