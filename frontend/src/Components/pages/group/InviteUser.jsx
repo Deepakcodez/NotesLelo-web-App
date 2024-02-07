25405d12b7e3f38bc6e843806a574e6af2477896
Deepak <deepak2603dk@gmail.com>
2024-02-07 23:40:54 +0530

    invite user frontend part done

--- frontend/src/Components/pages/group/InviteUser.jsx
+++ frontend/src/Components/pages/group/InviteUser.jsx
@@ -1,8 +1,43 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { createGroupContext } from '../../../Context';
 
 export const InviteUser = () => {
     const {  showInviteForm, setInviteForm} = useContext(createGroupContext);
+    const [emailValue, setEmailValue] = useState({email:""});
+    const [warning, setWarning] = useState("")
+    const [ warningMsg, setWarningmsg] = useState("")
+    
+
+         
+    const changeHandler=(e)=>{
+        console.log(e.target.value);
+        setEmailValue({email:e.target.value})
+        
+    }
+
+    // Function to check if email has a valid format
+const isValidEmail = (email) => {
+    // Regular expression for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+   const invite=(e)=>{
+    e.preventDefault()
+    const { email } = emailValue;
+    if (email.trim() === "") {
+        setWarningmsg("No Email provided");
+      } else if (!email.includes("@")) {
+        setWarningmsg("Invalid Email");
+      } else if (!isValidEmail(email)) {
+        setWarningmsg("Invalid Email format");
+    }
+      else{
+        //seding email to backend
+        alert("data sent")
+      }
+   }
+
 
   return (
     <>
@@ -19,7 +54,9 @@ export const InviteUser = () => {
             <div className="flex rounded-md shadow-sm ring-1 ring-inset focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
               <input
                 type="text"
-                name="id"
+                name="email"
+                value={emailValue.email}
+                onChange={changeHandler}
                 placeholder="User email who want to join..."
                 className="block flex-1 border-0 text-white bg-transparent py-1.5 pl-1 bg-gray-700   rounded-md text-white-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
               />
@@ -28,7 +65,7 @@ export const InviteUser = () => {
 
 
           <button
-         
+           onClick={invite}
            className="bg-blue-400 rounded md py-1.5 w-full mt-3 hover:bg-blue-500 hover:text-white ">
             Invite
           </button>
@@ -40,10 +77,10 @@ export const InviteUser = () => {
           </button>{" "}
         </form>
         <div className="h-[2rem]">
-        {/* {
+        {
           warning&&
-      <h1 className="warning text-slate-400">Group ID is required</h1>
-        } */}
+      <h1 className="warning text-slate-400">Something wrong!</h1>
+        }
 
         </div>
       </div>
