f4a49c40402e0dc9f3c67ad1d169d8285f94144c
Deepak <deepak2603dk@gmail.com>
2024-02-14 17:59:13 +0530

    fixing home page bug

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,22 +1,36 @@
 
+import { useEffect, useState } from "react"
 import { GroupCard } from "./shared/GroupCard"
 
 
 const Home = () => {
+const [isfetching , setFetching] = useState(false)
+    
+
+
+     const  fetchingCheck=(state)=>{
+      setFetching(state);
+      console.log('>>>>>>>>>>>',isfetching)
+
+     }
+
 
 
     return (
       <>
+ <div className={` ${isfetching ? "h-full w-full bg-slate-800 flex justify-center items-center "   : "pt-20 px-[1rem] md:px-[6rem] justify-center w-full overflow-y-scroll no-scrollbar pb-[30rem] h-auto  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8"}`}>
+        
+        <GroupCard fetching={fetchingCheck} />
+        {
+          isfetching&& 
+          <>
+          <div className="">
 
-        <div className="pt-20 px-[1rem] md:px-[6rem]  justify-center   w-full     overflow-y-scroll no-scrollbar  pb-[30rem] h-auto  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 "
-         
-
-        >
-          <GroupCard  />
-
-
-
-        </div>
+           <h1 className="text-white">nothing to show</h1>
+          </div>
+          </>
+        }
+      </div>
       </>
     )
   }
--- frontend/src/Components/shared/GroupCard.jsx
+++ frontend/src/Components/shared/GroupCard.jsx
@@ -5,12 +5,10 @@ import { useNavigate } from "react-router-dom";
 import { createGroupContext } from "../../Context";
 import { HomePageGhost } from "./ghost/HomePageGhost";
 
-
-
-export const GroupCard = () => {
+export const GroupCard = ({fetching}) => {
   const navigate = useNavigate();
   const { isCreateGroup, setCreateGroup } = useContext(createGroupContext);
- 
+  const [isLoading, setLoading] = useState(false);
   const userIdRefs = useRef([]);
   const [enlargeIcon, setEnlargeIcon] = useState(null);
   const [groups, setGroups] = useState([]);
@@ -18,8 +16,9 @@ export const GroupCard = () => {
 
   useEffect(() => {
     const fetchingAllGroup = async () => {
+      setLoading(true); // Set loading state to true
+    
       try {
-       
         const response = await axios.get(
           "https://notes-lelo-app-backend.vercel.app/api/v1/group/all",
           {
@@ -30,18 +29,27 @@ export const GroupCard = () => {
           }
         );
         setGroups(response.data.Groups);
-       
-  
+        if(!response.data.Groups.length){
+          fetching(true)
+        }
+        else{
+          fetching(false)
+        }
+      
       } catch (error) {
         console.log("Error fetching data:", error);
-   
+        
+      } finally {
+        setLoading(false); // Set loading state to false
+      
+
       }
     };
 
     fetchingAllGroup();
-  }, [ isCreateGroup,  ]);
+  }, [isCreateGroup]);
 
-  const copyIdHandler = (event , index) => {
+  const copyIdHandler = (event, index) => {
     try {
       event.stopPropagation();
       setEnlargeIcon(index);
@@ -57,53 +65,47 @@ export const GroupCard = () => {
   };
 
   const cardClickHandler = async (groupId) => {
-    // console.log('Clicked on Group ID:', groupId);
-
     navigate("/group/demand");
     localStorage.setItem("groupId", groupId);
   };
 
-
-  
-
-  if (!groups.length) {
-    return <HomePageGhost />;
-  }
-
-  return (
-    <>
-      {groups.map((group, index) => (
-        <Fragment key={index}>
-          <div
-            onClick={() => cardClickHandler(group._id)}
-            value={group._id}
-            className="card bg-slate-500/75 h-40 min-w-[auto] rounded-md shadow-lg border-[1px] border-t-slate-300 hover:border-t-slate-100 border-slate-400/50 hover:shadow-2xl hover:bg-slate-600 hover:scale-[1.009] outline-slate-200/5 outline-1 outline-offset-4 outline"
-          >
-            <div className="border-b-[1px] pt-2 rounded-t-md border-b-slate-400/70 bg-slate-700">
-              <h1 className="groupName px-4 font-bold text-white">
-                {group.title.toUpperCase()}
-              </h1>
-
-              <div className="flex items-center justify-between px-4 pb-2">
-                <input
-                  type="text"
-                  value={group._id}
-                  ref={(el) => (userIdRefs.current[index] = el)}
-                  disabled={true}
-                  className="groupID text-slate-500 text-sm bg-transparent"
-                />
-                <IoCopy
-                  className={`text-orange-200 hover:text-orange-300 ${
-                    enlargeIcon === index ? 'text-xl' : 'text-lg'
-                  } `}
-                  onClick={(event) => copyIdHandler(event, index)}
-                />
+  if (isLoading) {
+    console.log("Groups length:", groups.length);
+    return(<HomePageGhost/>)
+  } else {
+    return (
+      <>
+        {groups.map((group, index) => (
+          <Fragment key={index}>
+            <div
+              onClick={() => cardClickHandler(group._id)}
+              value={group._id}
+              className="card bg-slate-500/75 h-40 min-w-[auto] rounded-md shadow-lg border-[1px] border-t-slate-300 hover:border-t-slate-100 border-slate-400/50 hover:shadow-2xl hover:bg-slate-600 hover:scale-[1.009] outline-slate-200/5 outline-1 outline-offset-4 outline"
+            >
+              <div className="border-b-[1px] pt-2 rounded-t-md border-b-slate-400/70 bg-slate-700">
+                <h1 className="groupName px-4 font-bold text-white">
+                  {group.title.toUpperCase()}
+                </h1>
+                <div className="flex items-center justify-between px-4 pb-2">
+                  <input
+                    type="text"
+                    value={group._id}
+                    ref={(el) => (userIdRefs.current[index] = el)}
+                    disabled={true}
+                    className="groupID text-slate-500 text-sm bg-transparent"
+                  />
+                  <IoCopy
+                    className={`text-orange-200 hover:text-orange-300 ${enlargeIcon === index ? 'text-xl' : 'text-lg'
+                      } `}
+                    onClick={(event) => copyIdHandler(event, index)}
+                  />
+                </div>
               </div>
+              <h1 className="p-2 text-slate-200">{group.description}</h1>
             </div>
-            <h1 className="p-2 text-slate-200">{group.description}</h1>
-          </div>
-        </Fragment>
-      ))}
-    </>
-  );
-};
+          </Fragment>
+        ))}
+      </>
+    );
+  }
+}
