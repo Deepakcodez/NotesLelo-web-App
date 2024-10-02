f1a5e167173cde9ae61c8adcff94442049babe12
Deepak <deepak2603dk@gmail.com>
2024-10-02 16:12:45 +0530

    upd : improve file struc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,14 +9,14 @@ import Home from "./Components/Home";
 import { AuthLayout, SignIn, SignUp } from "./layouts/users/auth";
 import RootLayout from "./Components/RootLayout";
 import PageNotFound from "./Components/PageNotFound";
-import YourNotes from "./Components/pages/YourNotes";
-import Notification from "./Components/pages/Notification";
-import Saved from "./Components/pages/Saved";
-import Profile from "./Components/pages/Profile";
-import { Library } from "lucide-react";
 import { GroupChat, GroupDemand, Notes } from "./layouts/users/Group";
+import { Savedpage } from "./layouts/users/Saved";
+import { NotificationPage } from "./layouts/users/Notification";
+import { ProfilePage } from "./layouts/users/Profile";
+import { LibraryPage } from "./layouts/users/Library";
+import YourNotesPage from "./layouts/users/YourNotes/YourNotesPage";
 
-const App:React.FC=()=> {
+const App: React.FC = () => {
   return (
     <>
       <main className=" main flex h-screen ">
@@ -30,18 +30,18 @@ const App:React.FC=()=> {
 
           {/* private routes  */}
           <Route element={<RootLayout />}>
-            <Route  path="/" element={<Home />}></Route>
-            <Route path="/yourNotes" element={<YourNotes />}></Route>
-            <Route path="/notifications" element={<Notification />}></Route>
-            <Route path="/savedNotes" element={<Saved />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/library" element={<Library />}></Route>
-            <Route path="/group" element={<GroupChat/>}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/yourNotes" element={<YourNotesPage />}></Route>
+            <Route path="/notifications" element={<NotificationPage />}></Route>
+            <Route path="/savedNotes" element={<Savedpage />}></Route>
+            <Route path="/profile" element={<ProfilePage />}></Route>
+            <Route path="/library" element={<LibraryPage />}></Route>
+            <Route path="/group" element={<GroupChat />}>
               <Route path="demand" element={<GroupDemand />} />
               <Route path="notes" element={<Notes />} />
             </Route>
 
-            <Route path="*" element={<PageNotFound/>}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
           </Route>
         </Routes>
       </main>
