20e0bbef188f5a3c7de1cad09cf3c8d966fce180
muskan <muskanloach984@gmail.com>
2024-12-25 00:15:01 +0530

    add posts

--- frontend/src/pages/users/Library/component/Library.tsx
+++ frontend/src/pages/users/Library/component/Library.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import Lottie from "lottie-react";
-import loaderBook from  '../../../../assets/loaderbook.json';
-export const Library:React.FC = () => {
+import React from "react";
+
+import Posts from "./Post";
+
+export const Library: React.FC = () => {
   return (
     <>
-      <div className='h-full w-full flex flex-col  justify-center  items-center'>
-
-        <Lottie className='h-[5rem]' animationData={loaderBook} loop={true} />
-      <div className='text-white'>comming soon.....</div>
+      <div className="h-full w-full flex flex-col  justify-center  items-center">
+        <Posts />
       </div>
     </>
-  )
-}
+  );
+};
