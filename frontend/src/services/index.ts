78e99249e48653b4fb3b610425c745a0dd6513b7
Deepak <deepak2603dk@gmail.com>
2024-12-29 04:44:08 +0530

    fix

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,11 +1,11 @@
 import addLikeToDemand from "./addLikeToDemand";
 import addDisLikeToDemand from "./addDislikeToDemand";
 import deleteDemand from "./deleteDemand";
-import getPostComments from "./getpostComents";
+// import getPostComments from "./getpostComents";
 
 export{
     addLikeToDemand,
     addDisLikeToDemand,
     deleteDemand,
-    getPostComments
+    // getPostComments
 }
