f348de6d5aaff221d9cd11f0648c200297c77207
muskan <muskanloach984@gmail.com>
2024-12-30 17:05:10 +0530

    add animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,19 @@ export default {
 "./auth/**/*.{js,ts,jsx,tsx}"
 ],
   theme: {
-    extend: {},
-  },
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(1rem)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in": "fadeIn 0.5s ease-in-out forwards",
+      },
+    },
+    },
+
   plugins: [
 
     function ({addUtilities}){
