ddd877c74028a90748d71fd6531b41b66422c1e4
Deepak <deepak2603dk@gmail.com>
2025-06-12 01:27:00 +0530

    fix2

--- frontend/src/services/getuser.ts
+++ frontend/src/services/getuser.ts
@@ -4,7 +4,7 @@ const getUserById = async (userId: string | undefined) => {
   const base_url = import.meta.env.VITE_BASE_URL as string;
   let resp;
   try {
-    resp = await axios.get(`http://localhost:8000/api/v1/user/${userId}`);
+    resp = await axios.get(`${base_url}/api/v1/user/${userId}`);
   } catch (error) {
     throw error;
   }
